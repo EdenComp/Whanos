168488c59d3ea077b86bc90d04960d2e44cf3ba4
Florian Lauch <florian.lauchh@gmail.com>
2023-11-28 19:26:11 +0100

    feat: Python docker image

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -4,3 +4,4 @@ IMAGES_DIR=$GIT_ROOT_PATH/images
 docker build $IMAGES_DIR/c --file $IMAGES_DIR/c/Dockerfile.base -t whanos-c:latest
 docker build $IMAGES_DIR/c --file $IMAGES_DIR/java/Dockerfile.base -t whanos-java:latest
 docker build $IMAGES_DIR/c --file $IMAGES_DIR/javascript/Dockerfile.base -t whanos-javascript:latest
+docker build $IMAGES_DIR/c --file $IMAGES_DIR/python/Dockerfile.base -t whanos-python:latest

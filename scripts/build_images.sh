d593c5156a8e19b6e5725368453873dd0c9ecabc
Florian Lauch <florian.lauchh@gmail.com>
2023-11-28 18:26:05 +0100

    feat: Java and Javascript whanos images

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,3 +2,5 @@ GIT_ROOT_PATH=$(git rev-parse --show-toplevel)
 IMAGES_DIR=$GIT_ROOT_PATH/images
 
 docker build $IMAGES_DIR/c --file $IMAGES_DIR/c/Dockerfile.base -t whanos-c:latest
+docker build $IMAGES_DIR/c --file $IMAGES_DIR/java/Dockerfile.base -t whanos-java:latest
+docker build $IMAGES_DIR/c --file $IMAGES_DIR/javascript/Dockerfile.base -t whanos-javascript:latest

4d815bea94f8c9c1396dfbe158f707d3f78323ae
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-11-29 11:10:02 +0100

    feat: all folder

---
+++ jenkins/job_dsl.groovy
@@ -0,0 +1,127 @@
+folder('Whanos_base_images') {
+  displayName('Whanos base images')
+  description('Folder for Whanos base images')
+}
+
+freeStyleJob('Whanos_base_images/whanos-c') {
+  wrappers {
+    preBuildCleanup ()
+  }
+  parameters {
+    stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+  }
+  scm {
+    git {
+      remote {
+        url('\$GIT_REPOSITORY_URL')
+      }
+    }
+  }
+}
+
+freeStyleJob('Whanos_base_images/whanos-java') {
+  wrappers {
+    preBuildCleanup ()
+  }
+  parameters {
+    stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+  }
+  scm {
+    git {
+      remote {
+        url('\$GIT_REPOSITORY_URL')
+      }
+    }
+  }
+}
+
+freeStyleJob('Whanos_base_images/whanos-javascript') {
+  wrappers {
+    preBuildCleanup ()
+  }
+  parameters {
+    stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+  }
+  scm {
+    git {
+      remote {
+        url('\$GIT_REPOSITORY_URL')
+      }
+    }
+  }
+}
+
+freeStyleJob('Whanos_base_images/whanos-python') {
+  wrappers {
+    preBuildCleanup ()
+  }
+  parameters {
+    stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+  }
+  scm {
+    git {
+      remote {
+        url('\$GIT_REPOSITORY_URL')
+      }
+    }
+  }
+}
+
+freeStyleJob('Whanos_base_images/whanos-befunge') {
+  wrappers {
+    preBuildCleanup ()
+  }
+  parameters {
+    stringParam("GIT_REPOSITORY_URL", "", "Git URL of the repository to clone")
+  }
+  scm {
+    git {
+      remote {
+        url('\$GIT_REPOSITORY_URL')
+      }
+    }
+  }
+}
+
+folder('Projects') {
+  displayName('Projects')
+  description('Folder for projects')
+}
+
+folder('Whanos_base_images/Build_all_base_images') {
+  displayName('Build all base images')
+  description('Folder for build all base images')
+}
+
+folder ('link-project') {
+  displayName('link-project')
+  description('Folder for link-project')
+}
+
+// freeStyleJob('Tools/SEED') {
+//   parameters {
+//     stringParam("GITHUB_NAME", "", 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+//     stringParam("DISPLAY_NAME", "", "Display name for the job")
+//   }
+//   steps {
+//     dsl {
+//       text('''freeStyleJob("\$DISPLAY_NAME") {
+//         scm {
+//           github("\$GITHUB_NAME")
+//           triggers {
+//             scm('* * * * *')
+//           }
+//         }
+//         wrappers {
+//           preBuildCleanup()
+//         }
+//         steps {
+//           shell('make fclean')
+//           shell('make')
+//           shell('make tests_run')
+//           shell('make clean')
+//         }
+//       }'''.stripIndent())
+//     }
+//   }
+// }

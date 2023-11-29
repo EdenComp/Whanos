df33da08aa65cde1087a55ce7661a9ffe58c2d7a
Florian Lauch <florian.lauchh@gmail.com>
2023-11-29 15:17:26 +0100

    feat: Repository Deploy script

---
+++ scripts/repository_deploy.sh
@@ -0,0 +1,58 @@
+# Arguments handling
+if [ $# -eq 0 ] || [ $1 = "-h" ]; then
+    echo "Usage: $0 <repository_url> [branch]"
+    exit 1
+fi
+
+mkdir -p /tmp/whanos
+rm -rf /tmp/whanos/repository
+git clone $1 /tmp/whanos/repository && cd /tmp/whanos/repository
+
+if [ $# -eq 2 ]; then
+    git switch $2
+fi
+
+# Find if the repo is whanos-compatible
+VALID_CRITERIA=0
+REPO_NAME=$(basename -s .git `git config --get remote.origin.url`)
+
+if [ -f "app/main.bf" ]; then
+    VALID_CRITERIA=$((VALID_CRITERIA+1))
+    LANGUAGE="befunge"
+fi
+if [ -f "Makefile" ]; then
+    VALID_CRITERIA=$((VALID_CRITERIA+1))
+    LANGUAGE="c"
+fi
+if [ -f "app/pom.xml" ]; then
+    VALID_CRITERIA=$((VALID_CRITERIA+1))
+    LANGUAGE="java"
+fi
+if [ -f "package.json" ]; then
+    VALID_CRITERIA=$((VALID_CRITERIA+1))
+    LANGUAGE="javascript"
+fi
+if [ -f "requirements.txt" ]; then
+    VALID_CRITERIA=$((VALID_CRITERIA+1))
+    LANGUAGE="python"
+fi
+
+if [ $VALID_CRITERIA -eq 0 ]; then
+    echo "This repository is not whanos-compatible"
+    exit 1
+fi
+if [ $VALID_CRITERIA -gt 1 ]; then
+    echo "This repository matches multple whanos criteria, please remove the ambiguity"
+    exit 1
+fi
+
+# Build the image
+SCRIPT_DIR=$(realpath $(dirname $0))
+
+if [ -f "Dockerfile" ]; then
+  docker build . -t $REPO_NAME:latest
+else
+  docker build . --file $SCRIPT_DIR/../images/$LANGUAGE/Dockerfile.standalone -t $REPO_NAME:latest
+fi
+
+# TODO: Deploy image on docker registry + k8s
